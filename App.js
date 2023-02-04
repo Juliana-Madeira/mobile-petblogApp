b23ac230872dcd01644811776524c95adc8758b4
Juliana Madeira <juliwood1980@gmail.com>
2023-02-04 08:42:34 -0300

    fix: install react-async-storage

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import firebase from './src/firebaseConfig';
 
 
 import { NavigationContainer } from '@react-navigation/native';
--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAvAl0QL1Xqc7I5coRcMVqMBxekHZ0yltc",
